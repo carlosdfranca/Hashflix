545b8fcd1178b4eb8e7c7d8498fe9a89253a7a80
Carlos Eduardo <carlosdudu258@gmail.com>
2023-10-25 09:44:05 -0300

    Começando a estilizar o homepage

--- filme/context.py
+++ filme/context.py
@@ -2,11 +2,11 @@ from .models import Filme
 
 #Lista de Filmes mais recentes
 def lista_filmes_frequentes(request):
-    lista_filmes = Filme.objects.all().order_by("-data_criacao")[0:10]
+    lista_filmes = Filme.objects.all().order_by("-data_criacao")[0:8]
     return {"lista_filmes_recentes": lista_filmes}
 
 
 # Lista de Filmes em alta
 def lista_filmes_emalta(request):
-    lista_filmes = Filme.objects.all().order_by("-visualizacoes")[0:10]
+    lista_filmes = Filme.objects.all().order_by("-visualizacoes")[0:8]
     return {"lista_filmes_emalta": lista_filmes}
