194ab70ffdc2bf038edf53da34379e70e53a0ab4
Carlos Eduardo <carlosdudu258@gmail.com>
2023-11-01 12:26:33 -0300

    Config para o deploy

--- hashflix/settings.py
+++ hashflix/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=whr3zturr$*wdb-!t1q*jhow3jz9yxrw)jdx9hww=&9u6k!2l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,6 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
